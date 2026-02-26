79f8624ceac060ded935d1ff90834ac217d6c03b
bcrdaniel <bcr.daniel@gmail.com>
2026-02-26 11:35:07 -0300

    feat: Versao customizada baseada na master para resolver traducao br

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -105,6 +105,11 @@ class CeleryConfig:
 
 CELERY_CONFIG = CeleryConfig
 
+LANGUAGES = {
+    'en': {'flag': 'us', 'name': 'English'},
+    'pt_BR': {'flag': 'br', 'name': 'Portuguese (Brazil)'},
+}
+
 FEATURE_FLAGS = {"ALERT_REPORTS": True, "DATASET_FOLDERS": True}
 ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
 WEBDRIVER_BASEURL = f"http://superset_app{os.environ.get('SUPERSET_APP_ROOT', '/')}/"  # When using docker compose baseurl should be http://superset_nginx{ENV{BASEPATH}}/  # noqa: E501
